b64909bcffd17bb248e125670653988b8ba05605
Imelda Hope <imeldalarusso@gmail.com>
2024-09-17 12:52:18 +0300

    seeds db with faker data

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,23 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    #clear table game every time we seed to avoid duplicates. NB: Be careful with this since it empties the table
+    session.query(Game).delete()
+    session.commit()
+
+    # Add a console message so we can see output when the seed file runs
+print("Seeding games...")
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+
+for _ in range(50)]
+
+session.bulk_save_objects(games)
+session.commit()
